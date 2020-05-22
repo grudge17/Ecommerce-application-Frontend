52b36aaf852237e10bc7ae4cb40d8d16be739485
grudge17 <shresthpal17@gmail.com>
2020-05-22 16:06:33 +0530

    Shop Page with search filters

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -23,6 +23,14 @@ const Menu=({history})=>( //ul-unordered list
                   Home
               </Link>
           </li>
+          <li className="nav-item">
+              <Link 
+              className="nav-link" 
+              style={isActive(history,'/shop')} 
+              to="/shop">
+                  Shop
+              </Link>
+          </li>
           {isAuthenticated() && isAuthenticated().user.role===0 && (
               <li className="nav-item">
               <Link 
